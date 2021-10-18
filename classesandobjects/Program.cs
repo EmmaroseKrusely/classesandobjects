0ec627abb331979e87c35503e804093051555cff
Krusely_Emmarose <Krusely_Emmarose@G-02-24>
2021-10-18 10:07:50 -0400

    Add project files.

---
+++ classesandobjects/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesandobjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+            sam.SetHobbies(new string[] {
+          "listening to audiobooks and podcasts",
+          "playing rec sports like bowling and kickball",
+          "writing a speculative fiction novel",
+          "reading advice columns"
+      });
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+
+
+    class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(
+          string name,
+          int age,
+          string city,
+          string country,
+          string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string bio = $"Name: {name}\nAge:{age}\nCity: {city}\nCountry:
+          { country}\nPronouns: { pronouns}
+            " ;
+  
+
+          bio += "\nHobbies: \n";
+            foreach (string hobby in hobbies)
+            {
+                bio += $"- {hobby}\n"
+            }
+
+            return bio;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+
+    }
+}
